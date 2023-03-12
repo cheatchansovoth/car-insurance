378d391443f7aa6ccc58bf15672677584d49b69f
cheatchansovoth <57830826+cheatchansovoth@users.noreply.github.com>
2023-03-13 09:54:11 +1300

    update

--- src/component/Home.js
+++ src/component/Home.js
@@ -3,7 +3,15 @@ import React from 'react'
 export const Home = () => {
   return (
     <div className='w-screen h-screen'>
-        
+      <div className='h-[180px] min-w-full bg-blue-400'>
+      </div>
+      <div className='mx-3 space-y-3'>
+        <h1 className='text-3xl font-bold'>Car Insurance</h1>
+        <h1 className='text-2xl font-bold'>Price from <span className='text-3xl'>$175</span></h1>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+        <button className='btn bg-blue-400 px-3 py-2'>Get A quote</button>
+        <button className='btn bg-blue-400 px-3 py-2 mx-3 border-2 border-black'>Retrive a Quote</button>
+      </div>
     </div>
   )
 }
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -21,11 +21,11 @@ export const Navbar = () => {
               ease: [0, 0.71, 0.2, 1.01]
             }}
             >
-                <div className='flex flex-row h-[50px] items-center lg:flex lg:flex-row lg:justify-center lg:items-center text-center lg:my-[2%]'>
+                <div className='flex flex-row h-[50px] items-center  justify-between lg:flex lg:flex-row lg:justify-center lg:items-center text-center lg:my-[2%]'>
                     <div className='w-[50%]'>
                     <h1 className='text-2xl font-bold'><span className='text-sky-600'>Car</span>insurance</h1>
                     </div>
-                    <div className='my-6 h-[20%] hover:text-slate-500 lg:hidden'>
+                    <div className='my-6 h-[20%]  hover:text-slate-500 lg:hidden'>
                     <GiHamburgerMenu onClick={toggleShow}/>
                     </div>
                     <div className='lg:flex-row lg:justify-end lg:space-x-5 lg:space-y-0 lg:w-[70%] lg: mr-[10%] hidden lg:flex'>
@@ -34,14 +34,17 @@ export const Navbar = () => {
                         <a href='#'>Pricing</a>
                         <a href='#' className='w-[20%] bg-cyan-600 rounded-2xl p-1'>Try Now</a>
                     </div>
+                    <div className='lg:hidden'>
+                    <a href='#' className='w-[30%] bg-cyan-600 rounded-xl p-1'>Try Now</a>
+                    </div>
                 </div>
                  {show && 
                  (
-                    <motion.div className='flex flex-col items-center text-center space-y-3 justify-center lg:flex-row lg:justify-end lg:space-x-5 lg:space-y-0 lg:w-[70%] lg:mr-[10%] lg:flex lg:hidden'
+                    <motion.div className='flex flex-col items-center space-y-3 justify-center lg:flex-row lg:justify-end lg:space-x-5 lg:space-y-0 lg:w-[70%] lg:mr-[10%] lg:flex lg:hidden'
                     initial={{ opacity: 0, scale: 0.5 }}
                     animate={{ opacity: 1, scale: 1 }}
                     transition={{
-                      duration: 0.2,
+                      duration: 0.3,
                       delay: 0.1,
                       ease: [0, 0.71, 0.2, 1.01]
                     }}
@@ -49,7 +52,6 @@ export const Navbar = () => {
                     <a href='#'>Home</a>
                     <a href='#'>About</a>
                     <a href='#'>Pricing</a>
-                    <a href='#' className='w-[30%] bg-cyan-600 rounded-2xl p-1'>Try Now</a>
                     </motion.div>
                 )
                 }
